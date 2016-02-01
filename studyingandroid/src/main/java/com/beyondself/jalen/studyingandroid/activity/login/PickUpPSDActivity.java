0249e71aa473b667a0f69b7b9e18a250e72976c8
Jalen <1439504683@qq.com>
2016-02-01 22:17:33 +0800

    登录验证码核对功能完善

--- studyingandroid/src/main/java/com/beyondself/jalen/studyingandroid/activity/login/PickUpPSDActivity.java
+++ studyingandroid/src/main/java/com/beyondself/jalen/studyingandroid/activity/login/PickUpPSDActivity.java
@@ -1,16 +1,22 @@
 package com.beyondself.jalen.studyingandroid.activity.login;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.beyondself.jalen.studyingandroid.R;
 import com.beyondself.jalen.studyingandroid.utils.ToastUtils;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cn.bmob.v3.BmobSMS;
 import cn.bmob.v3.BmobUser;
@@ -23,6 +29,28 @@ public class PickUpPSDActivity extends AppCompatActivity {
     private EditText et_pickup_pnumber;//手机号
     private EditText et_pickup_newpsd;//新密码
     private EditText et_pickup_newpsd_confirm;//新密码确认
+    private TextView tv_getSmsCode;
+    private int Count = 60;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0:
+                    tv_getSmsCode.setText(Count + "后重新发送");
+                    tv_getSmsCode.setClickable(false);
+                    break;
+                case 1:
+                    tv_getSmsCode.setText("重新获取验证码");
+                    tv_getSmsCode.setClickable(true);
+                    break;
+                default:
+
+                    break;
+
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +64,7 @@ public class PickUpPSDActivity extends AppCompatActivity {
         et_pickup_pnumber = (EditText) findViewById(R.id.et_pickup_pnumber);
         et_pickup_newpsd = (EditText) findViewById(R.id.et_pickup_newpsd);
         et_pickup_newpsd_confirm = (EditText) findViewById(R.id.et_pickup_newpsd_confirm);
+        tv_getSmsCode = (TextView) findViewById(R.id.tv_getSmsCode);
     }
 
     /**
@@ -47,9 +76,27 @@ public class PickUpPSDActivity extends AppCompatActivity {
         String number = et_pickup_pnumber.getText().toString();
         BmobSMS.requestSMSCode(PickUpPSDActivity.this, number, "修改密码", new RequestSMSCodeListener() {
             @Override
-            public void done(Integer integer, BmobException e) {
+            public void done(Integer smsId, BmobException e) {
                 if (e == null) {//验证码发送成功
-                    Log.i("smile", "短信id：" + integer);//用于查询本次短信发送详情
+                    Log.i("smile", "短信id：" + smsId);//用于查询本次短信发送详情
+                    ToastUtils.showToast(PickUpPSDActivity.this, "短信已发送,请注意查收", Toast.LENGTH_SHORT);
+                    tv_getSmsCode.setText("短信已发送");
+                    final Timer timer = new Timer();
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            handler.sendEmptyMessage(0);
+
+                            Count--;
+                            if (Count < 0) {
+                                timer.cancel();
+                                Count = 60;
+                                handler.sendEmptyMessage(1);
+                            }
+                        }
+                    }, 0, 1000);
+                } else {
+                    ToastUtils.showToast(PickUpPSDActivity.this, "短信发送失败", Toast.LENGTH_SHORT);
                 }
             }
         });
--- studyingandroid/src/main/java/com/beyondself/jalen/studyingandroid/activity/login/RegisterActivity.java
+++ studyingandroid/src/main/java/com/beyondself/jalen/studyingandroid/activity/login/RegisterActivity.java
@@ -1,14 +1,21 @@
 package com.beyondself.jalen.studyingandroid.activity.login;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.beyondself.jalen.studyingandroid.R;
 import com.beyondself.jalen.studyingandroid.domain.UserInfo;
+import com.beyondself.jalen.studyingandroid.utils.ToastUtils;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cn.bmob.v3.BmobSMS;
 import cn.bmob.v3.exception.BmobException;
@@ -19,6 +26,8 @@ public class RegisterActivity extends AppCompatActivity {
 
     private EditText et_register_code;//手机验证码
     private EditText et_register_pnumber;//手机号
+    private TextView tv_getSmsCode;
+    private int Count = 60;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +35,30 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         et_register_pnumber = (EditText) findViewById(R.id.et_register_pnumber);
         et_register_code = (EditText) findViewById(R.id.et_register_code);
+        tv_getSmsCode = (TextView) findViewById(R.id.tv_getSmsCode);
 
     }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0:
+                    tv_getSmsCode.setText(Count + "后重新发送");
+                    tv_getSmsCode.setClickable(false);
+                    break;
+                case 1:
+                    tv_getSmsCode.setText("重新获取验证码");
+                    tv_getSmsCode.setClickable(true);
+                    break;
+                default:
+
+                    break;
+
+            }
+        }
+    };
 
     /**
      * 获取验证码
@@ -35,16 +66,35 @@ public class RegisterActivity extends AppCompatActivity {
      * @param view
      */
     public void getCode(View view) {
-         String number = et_register_pnumber.getText().toString();
+        String number = et_register_pnumber.getText().toString();
         BmobSMS.requestSMSCode(RegisterActivity.this, number, "手机注册", new RequestSMSCodeListener() {
             @Override
-            public void done(Integer integer, BmobException e) {
+            public void done(Integer smsId, BmobException e) {
                 if (e == null) {//验证码发送成功
-                    Log.i("smile", "短信id：" + integer);//用于查询本次短信发送详情
+                    Log.i("smile", "短信id：" + smsId);//用于查询本次短信发送详情
+                    ToastUtils.showToast(RegisterActivity.this, "短信已发送,请注意查收", Toast.LENGTH_SHORT);
+                    tv_getSmsCode.setText("短信已发送");
+                    final Timer timer = new Timer();
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            handler.sendEmptyMessage(0);
+
+                            Count--;
+                            if (Count < 0) {
+                                timer.cancel();
+                                Count = 60;
+                                handler.sendEmptyMessage(1);
+                            }
+                        }
+                    }, 0, 1000);
+                } else {
+                    ToastUtils.showToast(RegisterActivity.this, "短信发送失败", Toast.LENGTH_SHORT);
                 }
             }
         });
     }
+
 
     /**
      * 一键手机注册
@@ -54,7 +104,7 @@ public class RegisterActivity extends AppCompatActivity {
     public void phone_register(View view) {
         Toast.makeText(RegisterActivity.this, "点击", Toast.LENGTH_SHORT).show();
         String number = et_register_pnumber.getText().toString();
-         String code = et_register_code.getText().toString();
+        String code = et_register_code.getText().toString();
         final UserInfo user = new UserInfo();
         user.setMobilePhoneNumber(number);//设置手机号码（必填）
 //        user.setUsername(xxx);                  //设置用户名，如果没有传用户名，则默认为手机号码
